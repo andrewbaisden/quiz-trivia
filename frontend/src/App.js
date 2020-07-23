b66d756fd4ce85226118416f49a43dabfde32aac
Andrew Baisden <andrewbaisden@gmail.com>
2020-07-23 14:52:03 +0100

    quiz html setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,268 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [quiz, setQuiz] = useState([
+		{
+			questionOne: [
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'A',
+					answer: true,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'B',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'C',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'D',
+					answer: false,
+				},
+			],
+		},
+		{
+			questionTwo: [
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'A',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'B',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'C',
+					answer: true,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'D',
+					answer: false,
+				},
+			],
+		},
+		{
+			questionThree: [
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'A',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'B',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'C',
+					answer: true,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'D',
+					answer: false,
+				},
+			],
+		},
+		{
+			questionFour: [
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'A',
+					answer: true,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'B',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'C',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'D',
+					answer: false,
+				},
+			],
+		},
+		{
+			questionFive: [
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'A',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'B',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'C',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'D',
+					answer: true,
+				},
+			],
+		},
+		{
+			questionSix: [
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'A',
+					answer: true,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'B',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'C',
+					answer: false,
+				},
+				{
+					id: uuidv4(),
+					question: 'Lorem ipsum',
+					letter: 'D',
+					answer: false,
+				},
+			],
+		},
+	]);
+
+	console.log(quiz);
+
+	return (
+		<Fragment>
+			<main>
+				<section>
+					<h1>Quiz Trivia</h1>
+				</section>
+				<section>
+					<h2>Question 1 Heading</h2>
+					<div>
+						{quiz[0].questionOne.map((q) => (
+							<div key={q.id}>
+								<div>
+									<p>{q.letter}</p>
+									<p>{q.question}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<section>
+					<h2>Question 2 Heading</h2>
+					<div>
+						{quiz[1].questionTwo.map((q) => (
+							<div key={q.id}>
+								<div>
+									<p>{q.letter}</p>
+									<p>{q.question}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<section>
+					<h2>Question 3 Heading</h2>
+					<div>
+						{quiz[2].questionThree.map((q) => (
+							<div key={q.id}>
+								<div>
+									<p>{q.letter}</p>
+									<p>{q.question}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<section>
+					<h2>Question 4 Heading</h2>
+					<div>
+						{quiz[3].questionFour.map((q) => (
+							<div key={q.id}>
+								<div>
+									<p>{q.letter}</p>
+									<p>{q.question}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<section>
+					<h2>Question 5 Heading</h2>
+					<div>
+						{quiz[4].questionFive.map((q) => (
+							<div key={q.id}>
+								<div>
+									<p>{q.letter}</p>
+									<p>{q.question}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+				<section>
+					<h2>Question 6 Heading</h2>
+					<div>
+						{quiz[5].questionSix.map((q) => (
+							<div key={q.id}>
+								<div>
+									<p>{q.letter}</p>
+									<p>{q.question}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</section>
+			</main>
+		</Fragment>
+	);
+};
 
 export default App;
