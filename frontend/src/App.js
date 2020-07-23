28be152005a8d2f80fda9512ce31debba619b080
Andrew Baisden <andrewbaisden@gmail.com>
2020-07-23 15:30:08 +0100

    quiz css design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -178,83 +178,83 @@ const App = () => {
 
 	return (
 		<Fragment>
-			<main>
+			<main className="container mx-auto">
 				<section>
-					<h1>Quiz Trivia</h1>
+					<h1 className="text-6xl">Quiz Trivia</h1>
 				</section>
 				<section>
-					<h2>Question 1 Heading</h2>
-					<div>
+					<h2 className="text-2xl font-bold">Question 1 Heading</h2>
+					<div className="bg-purple-800 p-6 mb-6">
 						{quiz[0].questionOne.map((q) => (
 							<div key={q.id}>
-								<div>
-									<p>{q.letter}</p>
-									<p>{q.question}</p>
+								<div className="flex flex-row items-center">
+									<p className="bg-purple-100 p-6 mb-4">{q.letter}</p>
+									<p className="bg-purple-400 p-4 w-full">{q.question}</p>
 								</div>
 							</div>
 						))}
 					</div>
 				</section>
 				<section>
-					<h2>Question 2 Heading</h2>
-					<div>
+					<h2 className="text-2xl font-bold">Question 1 Heading</h2>
+					<div className="bg-red-800 p-6 mb-6">
 						{quiz[1].questionTwo.map((q) => (
 							<div key={q.id}>
-								<div>
-									<p>{q.letter}</p>
-									<p>{q.question}</p>
+								<div className="flex flex-row items-center">
+									<p className="bg-red-100 p-6 mb-4">{q.letter}</p>
+									<p className="bg-red-400 p-4 w-full">{q.question}</p>
 								</div>
 							</div>
 						))}
 					</div>
 				</section>
 				<section>
-					<h2>Question 3 Heading</h2>
-					<div>
+					<h2 className="text-2xl font-bold">Question 1 Heading</h2>
+					<div className="bg-orange-800 p-6 mb-6">
 						{quiz[2].questionThree.map((q) => (
 							<div key={q.id}>
-								<div>
-									<p>{q.letter}</p>
-									<p>{q.question}</p>
+								<div className="flex flex-row items-center">
+									<p className="bg-orange-100 p-6 mb-4">{q.letter}</p>
+									<p className="bg-orange-400 p-4 w-full">{q.question}</p>
 								</div>
 							</div>
 						))}
 					</div>
 				</section>
 				<section>
-					<h2>Question 4 Heading</h2>
-					<div>
+					<h2 className="text-2xl font-bold">Question 1 Heading</h2>
+					<div className="bg-yellow-800 p-6 mb-6">
 						{quiz[3].questionFour.map((q) => (
 							<div key={q.id}>
-								<div>
-									<p>{q.letter}</p>
-									<p>{q.question}</p>
+								<div className="flex flex-row items-center">
+									<p className="bg-yellow-100 p-6 mb-4">{q.letter}</p>
+									<p className="bg-yellow-400 p-4 w-full">{q.question}</p>
 								</div>
 							</div>
 						))}
 					</div>
 				</section>
 				<section>
-					<h2>Question 5 Heading</h2>
-					<div>
+					<h2 className="text-2xl font-bold">Question 1 Heading</h2>
+					<div className="bg-green-800 p-6 mb-6">
 						{quiz[4].questionFive.map((q) => (
 							<div key={q.id}>
-								<div>
-									<p>{q.letter}</p>
-									<p>{q.question}</p>
+								<div className="flex flex-row items-center">
+									<p className="bg-green-100 p-6 mb-4">{q.letter}</p>
+									<p className="bg-green-400 p-4 w-full">{q.question}</p>
 								</div>
 							</div>
 						))}
 					</div>
 				</section>
 				<section>
-					<h2>Question 6 Heading</h2>
-					<div>
+					<h2 className="text-2xl font-bold">Question 1 Heading</h2>
+					<div className="bg-blue-800 p-6 mb-6">
 						{quiz[5].questionSix.map((q) => (
 							<div key={q.id}>
-								<div>
-									<p>{q.letter}</p>
-									<p>{q.question}</p>
+								<div className="flex flex-row items-center">
+									<p className="bg-blue-100 p-6 mb-4">{q.letter}</p>
+									<p className="bg-blue-400 p-4 w-full">{q.question}</p>
 								</div>
 							</div>
 						))}
